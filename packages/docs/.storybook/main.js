59cd424184dd2b7ea4a6f84c8ef72636cd372cc0
fagnersro <fagner.hg40@gmail.com>
2023-05-08 02:28:53 -0300

    ci: add docs workflow 1.1

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,13 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/fagnersr-ui_design-system/'
+    }
+
+    return config
   }
 }
